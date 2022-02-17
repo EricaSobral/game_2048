9d6ebd34e62e9ec9d6be1602815f7106691d0479
ericasobral <erica.sobral95@gmail.com>
2022-02-17 14:42:03 -0300

    criando função de movimento para a direita

--- 2048/app.js
+++ 2048/app.js
@@ -28,3 +28,32 @@ function generate(){
    }else generate()
 }
 })
+
+//movendo para a direita
+function movieRight(){
+    for (let i=0; i< 16; i++){
+        if(i % 4 === 0){
+            let totalOne = square[i].innerHTML
+            let totalTwo = square[i+1].innerHTML
+            let totalThree = square[i+2].innerHTML
+            let totalFour = square[i+3].innerHTML
+            let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            
+            console.log(row)
+
+            let filteredRow = row.filter(num => num)
+            let missing = 4 - filteredRow.length
+            let zeros = Array(missing).fill(0)
+            let newRow = zeros.concat(filteredRow)
+            
+            squares[i].innerHTML  -= newRow[0]
+            squares[i+1].innerHTML  -= newRow[1]
+            squares[i+2].innerHTML  -= newRow[2]
+            squares[i+3].innerHTML  -= newRow[3]    
+        }
+
+    }
+}
+
+movieRight()
